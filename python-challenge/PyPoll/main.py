8213b309b3b6bd8b5c9e83c9775a373252c276fd
Rachel Dowdy <93873949+radowtay@users.noreply.github.com>
2022-01-27 13:56:50 -0600

    uploading files

---
+++ python-challenge/PyPoll/main.py
@@ -0,0 +1,77 @@
+import os
+import csv
+import pathlib
+import icecream as ic
+
+#access file here
+#election_data = os.path.join(pathlib.Path(__file__).parent.resolve(), "Resources", "election_data.csv")
+election_data = os.path.join(pathlib.Path(__file__).parent.resolve(), "Resources", "test_election_data.csv")
+# variables here; can start counters
+vote_count = 0
+percent_votes = []
+candidates = []
+num_votes = []
+#start doing stuff here
+with open(election_data, 'r') as csvfile:
+    csvreader = csv.reader(csvfile) #, delimiter = "," do I need this?
+    csv_header = next(csvreader)
+    for row in csvreader:
+        vote_count += 1 
+        #* The total number of votes cast
+        #print(f"Total Votes: {vote_count}")
+
+
+        #  * A complete list of candidates who received votes
+        #loop through each line and find unique candidate names
+        #learn more about icecream
+        ic(row[2], type(row[2]))
+        ic(candidates, type(candidates))
+        print("-"*25)
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            index = candidates.index(row[2])
+            num_votes.append(1)
+        else:
+            index = candidates.index(row[2])
+            num_votes[index] += 1
+
+#  * The percentage of votes each candidate won
+#loop through rows and tally votes for each candidate
+#translate to a percentage
+#I can't figure this part out
+    for votes in num_votes:
+        percentage = (votes/vote_count) * 100
+        percent_votes.append(percentage)
+
+#  * The winner of the election based on popular vote.
+#Determine candidate with most votes #????? also not right
+    winner = max(num_votes)
+    index = num_votes.index(winner)
+    winning_candidate = candidates[index]
+#print it all down here to look nicer
+print("Election Results")
+print("----------------")
+print(f"Total Votes: {vote_count}")
+print("----------------")
+for i in range(len(candidates)):
+    print(f"{candidates}: {percent_votes}% ({num_votes})")   #??? I know I did this wrong
+print("----------------")
+print(f"Winner: {winning_candidate}")
+print("----------------")
+
+
+
+
+# Exporting to .txt file
+with open('analysis.txt', 'w') as f:
+    f.write("Election Results\n")
+    f.write("----------------\n")
+    f.write(f"Total Votes: {vote_count}\n")
+    f.write("----------------\n")
+    for i in range(len(candidates)):
+        f.write(f"{candidates}: {percent_votes}% ({num_votes})\n")   #??? I know I did this wrong
+    f.write("----------------\n")
+    f.write(f"Winner: {winning_candidate}\n")
+    f.write("----------------\n")
+
+    
